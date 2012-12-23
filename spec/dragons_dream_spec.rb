b6a52ec28adf5380cdd49d72e5a69b431a4fce68
zakuni <kunio038@gmail.com>
2012-12-23 21:15:55 +0900

    start writing test

--- spec/dragons_dream_spec.rb
+++ spec/dragons_dream_spec.rb
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 require 'minitest/autorun'
 require 'minitest/reporters'
+require 'dragons_dream'
 
 MiniTest::Reporters.use! [MiniTest::Reporters::SpecReporter.new]
 
@@ -8,6 +9,28 @@ describe DragonsDream do
   describe "guide" do
     it "shows the way" do
       true
+    end
+  end
+end
+
+describe DragonsDream::TransferInfo do
+  it "can be created" do
+    DragonsDream::TransferInfo.new.must_be_instance_of DragonsDream::TransferInfo
+  end
+end
+
+describe DragonsDream::RailwayInfo do
+  before do
+    @railway = DragonsDream::RailwayInfo.new
+  end
+
+  it "can be created" do
+    DragonsDream::RailwayInfo.new.must_be_instance_of DragonsDream::RailwayInfo
+  end
+  
+  describe "getstations" do
+    it "has method getStations" do
+      @railway.must_respond_to("getStations")
     end
   end
 end
