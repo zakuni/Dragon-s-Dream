1d1849d04dfaf081b2f59a2a6e9221cc15efa209
zakuni <kunio038@gmail.com>
2012-12-23 17:20:19 +0900

    added obvious test

---
+++ spec/dragons_dream_spec.rb
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+require 'minitest/autorun'
+require 'minitest/reporters'
+
+MiniTest::Reporters.use! [MiniTest::Reporters::SpecReporter.new]
+
+describe DragonsDream do
+  describe "guide" do
+    it "shows the way" do
+      true
+    end
+  end
+end
